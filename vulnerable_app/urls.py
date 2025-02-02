19199528a31ef3b0ed288c2095c2776f269ff48e
sa-k-ra <c1531055@eng.kitakyu-u.ac.jp>
2025-02-02 19:01:10 +0900

    ポートスキャン　脆弱性あり

--- vulnerable_app/urls.py
+++ vulnerable_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'vulnerable_app'
 urlpatterns = [
-    path('search/', views.search_user, name='search_user'),
+    path('', views.search_user, name='search_user'),
 ]
 
